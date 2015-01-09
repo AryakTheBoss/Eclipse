2c6bf9069231bb9fe9b99b070882e111a662309d
310843 <310843@HSS-101-07.westwindsor.k12.nj.us>
2015-01-09 08:48:15 -0500

    api

--- Encryption API/src/com/aryaktheboss/algorithims/DESede.java
+++ Encryption API/src/com/aryaktheboss/algorithims/DESede.java
@@ -7,6 +7,16 @@ package com.aryaktheboss.algorithims;
  * @author UnityBoss
  *
  */
-public class DESede {
+public class DESede implements Encryptor{
+
+	public String encrypt(String text) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String decrypt(String data) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
