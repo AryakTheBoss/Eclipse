8704bd46f463958cbaad7181ef750a2bb84b5cc0
AryakTheBoss <p.aryak@yahoo.com>
2015-01-09 17:46:48 -0500

    api fixed

---
+++ Encryption API/src/com/aryaktheboss/algorithims/KeySize.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package com.aryaktheboss.algorithims;
+
+/**
+ * @author UnityBoss
+ *
+ */
+public enum KeySize {
+	
+	/**
+	 * Smallest and Least secure key length of 
+	 * 1024 bits
+	 */
+	SMALL,
+	/**
+	 * Medium secure key length of 
+	 * 2048 bits
+	 */
+	MEDIUM,
+	/**
+	 * Large key size of 4096 bits
+	 */
+	LARGE,
+	/**
+	 * Highest Security key with length of
+	 * 8192 bits
+	 */
+	XTRALARGE
+	
+	
+	
+}
