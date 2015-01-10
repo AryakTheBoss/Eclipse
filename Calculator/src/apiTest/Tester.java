a3e8de7b000a7792b64bb83e40b1d9ff9f14c86d
AryakTheBoss <p.aryak@yahoo.com>
2015-01-09 19:03:41 -0500

    loiol

---
+++ Calculator/src/apiTest/Tester.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+package apiTest;
+
+
+
+import com.aryaktheboss.exceptions.InvalidKeyException;
+
+/**
+ * @author UnityBoss
+ *
+ */
+public class Tester {
+
+	/**
+	 * @param args
+	 * @throws InvalidKeyException 
+	 */
+	public static void main(String[] args) throws Exception, InvalidKeyException {
+		// TODO Auto-generated method stub
+        System.err.println("RSA Encryption: ");
+		RSA rsa = new RSA(KeySize.SMALL);
+		String enc = rsa.encrypt("Secret Phrase");
+		System.out.println("Encrypted Value: "+enc);
+		System.out.println("Decrypted Value: "+rsa.decrypt(enc));
+		System.err.println("AES Encryption: ");
+		
+		
+		AES aes = new AES();
+		String enc2 = aes.encrypt("Secret Phrase");
+		System.out.println("Encrypted Value: "+enc2);
+		System.out.println("Decrypted Value: "+aes.decrypt(enc2));		
+		System.err.println("DES Encryption: ");
+		
+		DES des = new DES();
+		String enc3 = des.encrypt("Secret Phrase");
+		System.out.println("Encrypted Value: "+enc3);
+		System.out.println("Decrypted Value: "+des.decrypt(enc3));
+		System.err.println("DESede Encryption: ");
+		
+		DESede tdes = new DESede(true);
+		String enc4 = tdes.encrypt("Secret Phrase");
+		System.out.println("Encrypted Value: "+enc4);
+		System.out.println("Decrypted Value: "+tdes.decrypt(enc4));
+		
+	}
+
+}
