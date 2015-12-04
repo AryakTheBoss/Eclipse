6de4b21945e87ad87bdce4675e4ff8e4060648b2
17ap0497 <17ap0497@HSS-101-16.westwindsor.k12.nj.us>
2015-12-04 13:52:20 -0500

    butsbts

--- Pande,Aryak/src/cards/CardTester.java
+++ Pande,Aryak/src/cards/CardTester.java
@@ -13,7 +13,24 @@ public class CardTester {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("test");
+		Deck d = new Deck();
+		System.out.println(d);
+		System.out.println("**********CUT**********");
+		d.cut();
+		System.out.println(d);
+		System.out.println("**********BridgeShuffle**********");
+		d.bridgeShuffle();
+		System.out.println(d);
+		System.out.println("**********CompleteShuffle**********");
+		d.completeShuffle();
+		System.out.println(d);
+		System.out.println("Dealing..................");
+		System.out.print(d.deal()+" ");
+		System.out.print(d.deal()+" ");
+		System.out.print(d.deal()+" ");
+		System.out.print(d.deal()+" ");
+		System.out.print(d.deal());
 
 	}
 
--- Pande,Aryak/src/cards/Deck.java
+++ Pande,Aryak/src/cards/Deck.java
@@ -11,29 +11,29 @@ import java.util.Stack;
  *
  */
 public class Deck {
-	
-	private Stack<Card> deck; 
 
-	public Deck() { 
+	private Stack<Card> deck;
 
-	deck = new Stack<Card>(); 
+	public Deck() {
+		deck = new Stack<Card>();
+		loadDeck();
+		//System.out.println(deck);
 
-	loadDeck(); 
+	}
 
-	} 
+	public Stack<Card> getDeck() {
 
-	public Stack<Card> getDeck() { 
+		return deck;
 
-	return deck; 
+	}
 
-	} 
 
-	// Load the stack with 52 cards in order 
+	// Load the stack with 52 cards in order
 
-	public void loadDeck(){
-		for(int i=1;i<5;i++){
-			for(int j=2;j<15;j++){
-				deck.push(new Card(i,j));
+	public void loadDeck() {
+		for (int i = 1; i < 5; i++) {
+			for (int j = 2; j < 15; j++) {
+				deck.push(new Card(i, j));
 			}
 		}
 	}
@@ -41,42 +41,80 @@ public class Deck {
 	public String toString() {
 		StringBuilder s = new StringBuilder();
 		int x = 0;
-		for(Card c : deck){
-			if(x == 4){
+		for (Card c : deck) {
+			//System.out.println(c);
+			if (x == 4) {
 				x = 0;
 				s.append("\n");
 			}
-			s.append(c);
+			s.append(c.toString()+"\t");
+			x++;
 		}
 		return s.toString();
-	} 
+	}
 
-	// Return and remove the top card 
+	// Return and remove the top card
 
 	public Card deal() {
 		return deck.pop(); // :^)
 	}
-	
 
-	// Take the top half of the deck (26 cards) and alternate card by card with 
+	// Take the top half of the deck (26 cards) and alternate card by card with
 
-	// the bottom half 
+	// the bottom half
 
-	public void bridgeShuffle() {} 
-
-	// split the deck at a random spot. Put the stack of cards above the random 
-
-	// spot below the other cards 
-//TODO f
-	public void cut() {
-		int r = 1 + (int)(Math.random()*52); 
-		Stack<Card> rekt = new Stack<Card>();
-		while(r>0){
+	public void bridgeShuffle() {
+		int r = 26;
+		Stack<Card> new1 = new Stack<Card>();
+		Stack<Card> alpha = new Stack<Card>();
+		while (r > 0) {
+			
+			new1.push(deck.pop());
+			r--;
+		}
+		for(int i=52;i>0;i--){
+			if(i % 2 == 0)
+				alpha.push(deck.pop());
+			else
+				alpha.push(new1.pop());
 			
 		}
-	} 
+		deck = alpha;
+		
+	}
 
-	// complete a bridge shuffle and then cut the deck. Repeat 7 times 
+	// split the deck at a random spot. Put the stack of cards above the random
 
-	public void completeShuffle() {}
+	// spot below the other cards
+	// TODO f
+	public void cut() {
+		int r = 1 + (int) (Math.random() * 52);
+		Stack<Card> new1 = new Stack<Card>();
+		Stack<Card> alpha = new Stack<Card>();
+
+		while (r > 0) {
+			new1.push(deck.pop());
+			r--;
+		}
+
+		for (Card c : new1)
+			alpha.push(c);
+		Collections.reverse(alpha);
+		for (Card c : deck)
+			alpha.push(c);
+
+		deck = alpha;
+
+	}
+
+	// complete a bridge shuffle and then cut the deck. Repeat 7 times
+
+	public void completeShuffle() {
+		// it's the industry standard
+		for(int i=0;i<7;i++){
+			bridgeShuffle();
+			cut();
+		}
+	}
+
 }
