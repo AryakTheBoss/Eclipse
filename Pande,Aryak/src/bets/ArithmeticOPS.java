5dbd18095f6fe2cfde4628028a3f37ab46f1c518
17cc0151 <17cc0151@HSS-101-17.westwindsor.k12.nj.us>
2015-12-09 12:45:50 -0500

    hwehwe

--- Pande,Aryak/src/bets/ArithmeticOPS.java
+++ Pande,Aryak/src/bets/ArithmeticOPS.java
@@ -24,53 +24,49 @@ public class ArithmeticOPS {
 	Vector<String> queue = new Vector<String>();
 
 	public ArithmeticOPS(String arg) {
-		ops = null;
-		nums = null;
 		input = arg;
 		splitInput();
 		calculate();
 	}
 
 	private void calculate() {
-		
+
 		while (!nums.empty()) {
 			int num = Integer.parseInt(nums.remove(0));
 			String sign = nums.remove(0);
 			int x = 0;
-			while(!ops.isEmpty()) {
-				if(ops.size() > 1 && orderOfOps.indexOf(ops.get(0)) > orderOfOps.indexOf(ops.get(1))) {
-					nums.add(x, runNumbers(nums.remove(x),nums.remove(x),ops.remove(x + 1)));
-					x = 0;
-				}
-				else if (ops.size() == 1) {
-					nums.add(x, runNumbers(nums.remove(x),nums.remove(x),ops.remove(x + 1)));
-				} 
-				else {
-					x++;
-				}
+			while (!ops.isEmpty()) {
+				String op = ops.remove(0);
+				if (op.equals("*")) {
 					
-				
+				} else if (op.equals("/")) {
+
+				} else if (op.equals("+")) {
+
+				} else if (op.equals("-")) {
+
+				}
+
 			}
-			
 
 		}
 
 	}
-	
+
 	private String runNumbers(String num1, String num2, String op) {
 		int num1i = Integer.parseInt(num1);
 		int num2i = Integer.parseInt(num2);
-		if(op == "^")
-			return Math.pow(num1i, num2i)+"";
-		else if(op == "*")
-			return (num1i*num2i)+"";
-		else if(op == "/")
-			return (num1i/num2i)+"";
-		else if(op == "+")
-			return (num1i+num2i)+"";
-		else if(op == "-")
-			return (num1i-num2i)+"";
-		
+		if (op == "^")
+			return Math.pow(num1i, num2i) + "";
+		else if (op == "*")
+			return (num1i * num2i) + "";
+		else if (op == "/")
+			return (num1i / num2i) + "";
+		else if (op == "+")
+			return (num1i + num2i) + "";
+		else if (op == "-")
+			return (num1i - num2i) + "";
+
 		return "Some shit went down";
 	}
 
