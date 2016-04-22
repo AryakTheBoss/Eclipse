b143548938c0586f967d9a3e29e50621bfe187a0
ARYAK <ARYAK@10.0.0.17>
2016-04-22 17:30:50 -0400

    added globals

--- Alarm Clock for RBPi/src/com/anaelectronics/Run.java
+++ Alarm Clock for RBPi/src/com/anaelectronics/Run.java
@@ -5,6 +5,13 @@ package com.anaelectronics;
 
 import com.anaelectronics.listeners.KeyboardListener;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Date;
+
+import org.apache.commons.net.ntp.NTPUDPClient;
+import org.apache.commons.net.ntp.TimeInfo;
 /**
  * @author ARYAK
  *
@@ -13,11 +20,25 @@ public class Run {
 
 	/**
 	 * @param args
+	 * @throws IOException 
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException  {
 		// TODO Auto-generated method stub
-		
+		getTimeFromServer();
 		
 	}
-
+	private static void getTimeFromServer() throws IOException{
+		
+		String TIME_SERVER = "time-c.nist.gov";   
+		NTPUDPClient timeClient = new NTPUDPClient();
+		InetAddress inetAddress = InetAddress.getByName(TIME_SERVER);
+		TimeInfo timeInfo = timeClient.getTime(inetAddress);
+		long returnTime = timeInfo.getMessage().getTransmitTimeStamp().getTime();
+		Date time = new Date(returnTime);
+		System.out.println(time.toString());
+		String[] ti = time.toString().split(" ")[3].split(":");
+		Globals.time = (Integer.parseInt(ti[0])*100)+(Integer.parseInt(ti[1]));
+		System.out.println(Globals.time);
+		
+	}
 }
---
+++ Alarm Clock for RBPi/src/com/anaelectronics/listeners/TimeCounter.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.anaelectronics.listeners;
+
+/**
+ * @author ARYAK
+ *
+ */
+public class TimeCounter implements Runnable{
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
