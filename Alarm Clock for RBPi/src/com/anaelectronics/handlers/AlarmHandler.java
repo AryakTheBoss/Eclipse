f835fdd6cb5dd3cd36b45c0b4a9724c9e126eee8
ARYAK <ARYAK@10.0.0.17>
2016-04-24 15:23:34 -0400

    Added setup for the color code dialog

--- Alarm Clock for RBPi/src/com/anaelectronics/handlers/AlarmHandler.java
+++ Alarm Clock for RBPi/src/com/anaelectronics/handlers/AlarmHandler.java
@@ -9,6 +9,10 @@ import java.util.Queue;
 
 import com.anaelectronics.AlarmClock;
 import com.anaelectronics.Globals;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
 import java.io.File;
 
 
@@ -19,6 +23,8 @@ import javax.sound.sampled.DataLine;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.SourceDataLine;
 import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 /**
  * @author ARYAK
@@ -29,17 +35,50 @@ public class AlarmHandler implements Runnable{
 
 	private static Thread t = null;
 	private static JButton snooze = new JButton("SNOOZE");
-	private static JButton redButton = new JButton();
-	private static JButton greenButton = new JButton();
-	private static JButton blueButton = new JButton();
-	private static JButton yellowButton = new JButton();
-	private static final char[] buttons = {'R','G','B','Y'};
+	private static JButton redButton = new JButton("R");
+	private static JButton greenButton = new JButton("G");
+	private static JButton blueButton = new JButton("B");
+	private static JButton orangeButton = new JButton("O");
+	private static JPanel colorButtons = new JPanel();
+	public static JFrame dialog = new JFrame("ALARM");
+	private static final char[] buttons = {'R','G','B','O'};
 	private static Queue<Character> buttonPresses = new LinkedList<Character>();
 	
 @Override
 public void run() {
 	// TODO Auto-generated method stub
 	
+	colorButtons.add(redButton);
+	colorButtons.add(greenButton);
+	colorButtons.add(blueButton);
+	colorButtons.add(orangeButton);
+	dialog.setSize(250, 140); 
+	dialog.setResizable(false);
+	Dimension dim = new Dimension(320,240);
+    dialog.setLocation(dim.width/2-dialog.getSize().width/2, dim.height/2-dialog.getSize().height/2);
+    dialog.setAutoRequestFocus(true);
+    redButton.setPreferredSize(new Dimension(50,50));
+    greenButton.setPreferredSize(new Dimension(50,50));
+    blueButton.setPreferredSize(new Dimension(50,50));
+    orangeButton.setPreferredSize(new Dimension(50,50));
+    redButton.setForeground(Color.RED);   
+    redButton.setBackground(Color.RED); 
+    redButton.setOpaque(true);
+    greenButton.setForeground(Color.GREEN);
+    greenButton.setBackground(Color.GREEN); 
+    greenButton.setOpaque(true);
+    blueButton.setForeground(Color.BLUE);
+    blueButton.setBackground(Color.BLUE); 
+    blueButton.setOpaque(true);
+    orangeButton.setForeground(Color.ORANGE);
+    orangeButton.setBackground(Color.ORANGE); 
+    orangeButton.setOpaque(false);
+    dialog.setLayout(new BorderLayout());
+    dialog.add(colorButtons, BorderLayout.SOUTH);
+    snooze.setEnabled(!Globals.snoozed);
+    dialog.add(snooze, BorderLayout.NORTH);
+    
+    dialog.setVisible(true);
 	Thread thisThread = Thread.currentThread();
 	
 	while(t == thisThread){
@@ -47,8 +86,10 @@ public void run() {
 		
 		//Manage when to ring alarm
 		//Display the button punch dialog or snooze once
+		//call dialog.setVisible(true);
 		
 	}
+	dialog.dispose();
 	
 }
 
