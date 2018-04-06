fb208732c823b47c85c0dac7b3af1881281d8dda
parya <parya@Mak-Surface>
2018-04-06 12:18:10 -0400

    Player Side now FULLY works

--- Experiment666 Damage Calc/src/somesexyshit/DamageCalculator.java
+++ Experiment666 Damage Calc/src/somesexyshit/DamageCalculator.java
@@ -41,11 +41,11 @@ LUCK: 25
  */
 public class DamageCalculator {
 	
-	public static final int[] soldierBS = {60,40,70,35,10,3,8}; 
-	public static final int[] bruiserBS = {25,48,30,30,20,10,10};
+	public static final int[] soldierBS = {60,40,70,35,10,3,8}; //UPDATED
+	public static final int[] bruiserBS = {25,60,30,30,20,10,10};
 	public static final int[] doctorBS = {35,28,35,38,25,6,12};
-	public static final int[] wizardBS = {30,20,15,10,30,5,5};
-	public static final int[] banditBS = {30,40,25,50,20,9,25};
+	public static final int[] wizardBS = {20,15,15,10,30,5,5}; //UPDATED
+	public static final int[] banditBS = {30,40,35,50,20,9,25};//UPDATED
 	public static final int HP=0,ATK=1,DEF=2,INV=3,SPATK=4,MVMT=5,LUCK=6;
 	public static int rawPlayerHealth=0;
 	public static int displayPlayerHealth=0;
@@ -54,45 +54,53 @@ public class DamageCalculator {
 	public static JProgressBar playerHealth;
 	public static JProgressBar enemyHealth;
 	public static JComboBox<Integer> level;
+	public static JComboBox<String> userClass;
 	public static JLabel H = new JLabel();
+	public static int[] currentScaledStat = new int[7];
+	public static int[] enemyStats = new int[7];
 	
-	
-	public static int calculateAttackDamage(int[] user, int[] receiver, int weaponDamage,int level) {
+	public static void levelScaleStats(String classType, int level) {
 		
-		if(Arrays.equals(user, soldierBS) && level != 0)
-			return ((user[ATK]*Math.round((float)Math.pow(1.10, level-1)))*3*weaponDamage)/receiver[DEF];
-		else if(Arrays.equals(user, wizardBS) && level != 0)
-			return ((user[ATK]*Math.round((float)Math.pow(1.20, level-1)))*3*weaponDamage)/receiver[DEF];
-		else
-			return ((user[ATK]*Math.round((float)Math.pow(1.15, level-1)))*3*weaponDamage)/receiver[DEF];
-		
-	}
-	public static int calculateSpAttackDamage(int[] user, int[] receiver, int attackPower,int level) {
-		
-		if(Arrays.equals(user, soldierBS) && level != 0)
-			return ((user[SPATK]*Math.round((float)Math.pow(1.10, level-1)))*3*attackPower)/receiver[DEF];
-		else if(Arrays.equals(user, wizardBS) && level != 0)
-			return ((user[SPATK]*Math.round((float)Math.pow(1.20, level-1)))*3*attackPower)/receiver[DEF];
-		else
-			return ((user[SPATK]*Math.round((float)Math.pow(1.15, level-1)))*3*attackPower)/receiver[DEF];
-		
-	}
-	public static void applyPlayerHealthStat(String classType, int level) {
-		System.out.println(classType);
 		if(classType.equals("Soldier")) {
-			rawPlayerHealth = soldierBS[HP]*Math.round((float)Math.pow(1.10, level-1))*25;
+			
+			for(int i=0;i<soldierBS.length;i++) {
+				currentScaledStat[i] = (int) (Math.ceil(soldierBS[i]*Math.pow(1.10, level-1)));
+			}
 			
 		}else if(classType.equals("Bruiser")) {
-			rawPlayerHealth = bruiserBS[HP]*Math.round((float)Math.pow(1.15, level-1))*25;
+			for(int i=0;i<soldierBS.length;i++) {
+				currentScaledStat[i] = (int) (Math.ceil(bruiserBS[i]*Math.pow(1.10, level-1)));
+			}
 		}else if(classType.equals("Wizard")) {
-			rawPlayerHealth = wizardBS[HP]*Math.round((float)Math.pow(1.20, level-1))*25;
+			for(int i=0;i<soldierBS.length;i++) {
+				currentScaledStat[i] = (int) (Math.ceil(wizardBS[i]*Math.pow(1.15, level-1)));
+			}
 		}else if(classType.equals("Doctor")) {
-			rawPlayerHealth = doctorBS[HP]*Math.round((float)Math.pow(1.15, level-1))*25;
+			for(int i=0;i<soldierBS.length;i++) {
+				currentScaledStat[i] = (int) (Math.ceil(doctorBS[i]*Math.pow(1.10, level-1)));
+			}
 		}else if(classType.equals("Bandit")) {
-			rawPlayerHealth = banditBS[HP]*Math.round((float)Math.pow(1.15, level-1))*25;
+			for(int i=0;i<soldierBS.length;i++) {
+				currentScaledStat[i] = (int) (Math.ceil(banditBS[i]*Math.pow(1.10, level-1)));
+			}
 		}
+		applyPlayerHealth();
+		
+	}
+	public static int calculateAttackDamage(int[] user, int[] enemy, int weaponDamage,int level) {
+		
+		return 0;
+		
+	}
+	public static int calculateSpAttackDamage(int[] user, int[] enemy, int attackPower,int level) {
+		
+		return 0;
+		
+	}
+	public static void applyPlayerHealth() {
+		rawPlayerHealth = currentScaledStat[HP]*25;
 		displayPlayerHealth = rawPlayerHealth/5;
-		System.out.println(displayPlayerHealth);
+	//	System.out.println(displayPlayerHealth);
 		playerHealth.setMaximum(displayPlayerHealth);
 		playerHealth.setValue(displayPlayerHealth);
 		H.setText("HP: "+displayPlayerHealth+"/"+playerHealth.getMaximum());
@@ -125,11 +133,12 @@ public class DamageCalculator {
 		main.setSize(1000, 500);
 		JPanel leftPanel = new JPanel();
 		JPanel rightPanel = new JPanel();
-		leftPanel.setSize(250, 500);
+		leftPanel.setSize(270, 500);
 		rightPanel.setSize(450, 500);
 		//JLabel comboLabel = new JLabel("Player Level");
 	
-		JTextField weaponAttack = new JTextField("Weapon Attack");
+		JTextField weaponAttack = new JTextField("Power");
+		
 		JButton pAttackButton = new JButton("Attack");
 		JButton eAttackButton = new JButton("Attack");
 		playerHealth.setMaximum(displayPlayerHealth);
@@ -141,16 +150,18 @@ public class DamageCalculator {
 		for(int i=1;i<=25;i++) {
 			level.addItem(i);
 		}
-		JComboBox<String> userClass = new JComboBox<String>();
+		userClass = new JComboBox<String>();
 		userClass.addItem("Soldier");
 		userClass.addItem("Bruiser");
 		userClass.addItem("Doctor");
 		userClass.addItem("Wizard");
 		userClass.addItem("Bandit");
 		JCheckBox spatk = new JCheckBox("is a Speical Attack?");
-		JLabel j = new JLabel("Player");
+		JLabel j = new JLabel("Level: ");
+		JLabel jj = new JLabel("Class: ");
 		leftPanel.add(j);
 		leftPanel.add(level,JPanel.TOP_ALIGNMENT);
+		leftPanel.add(jj);
 		leftPanel.add(userClass);
 		leftPanel.add(weaponAttack);
 		leftPanel.add(playerHealth,JPanel.BOTTOM_ALIGNMENT);
@@ -161,12 +172,13 @@ public class DamageCalculator {
 		main.add(leftPanel);
 		
 		
-		
+		levelScaleStats("Soldier", 1);
 		main.add(rightPanel);
 		main.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	    applyPlayerHealthStat("Soldier", 1);
+	    applyPlayerHealth();
 		main.setVisible(true);
 		userClass.addItemListener(new TheHandler());
+		level.addItemListener(new TheHandler2());
 		pAttackButton.addActionListener(new ActionListener() { 
 			 
 			
@@ -204,7 +216,26 @@ class TheHandler implements ItemListener{
             if (source instanceof JComboBox) {
                 JComboBox cb = (JComboBox)source;
                 Object selectedItem = cb.getSelectedItem();
-                DamageCalculator.applyPlayerHealthStat((String)selectedItem, (Integer)DamageCalculator.level.getSelectedItem());
+               // DamageCalculator.applyPlayerHealthStat((String)selectedItem, (Integer)DamageCalculator.level.getSelectedItem());
+               DamageCalculator.levelScaleStats((String)selectedItem, (Integer)DamageCalculator.level.getSelectedItem());
+               
+            }
+        }   
+    }
+
+	
+}
+class TheHandler2 implements ItemListener{
+	@Override
+    public void itemStateChanged(ItemEvent event) {
+
+        if(event.getStateChange() == ItemEvent.SELECTED) {
+            Object source = event.getSource();
+            if (source instanceof JComboBox) {
+                JComboBox cb = (JComboBox)source;
+                Object selectedItem = cb.getSelectedItem();
+               // DamageCalculator.applyPlayerHealthStat((String)selectedItem, (Integer)DamageCalculator.level.getSelectedItem());
+               DamageCalculator.levelScaleStats((String)DamageCalculator.userClass.getSelectedItem(), (Integer)selectedItem);
                
             }
         }   
---
+++ Experiment666 Damage Calc/src/somesexyshit/TestSomeShit.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package somesexyshit;
+
+/**
+ * @author parya
+ *
+ */
+public class TestSomeShit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(levelStat(3,60));
+	}
+	public static double levelStat(int level,int baseStat) {
+		
+		return Math.ceil(baseStat*Math.pow(1.10, level-1)); 
+		
+		
+	}
+
+}
