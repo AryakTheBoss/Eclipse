bed76849960284e986f9415fc65f4c6ae8c8dc33
parya <parya@Mak-Surface>
2018-04-06 12:22:26 -0400

    fixed imports

--- Experiment666 Damage Calc/src/somesexyshit/DamageCalculator.java
+++ Experiment666 Damage Calc/src/somesexyshit/DamageCalculator.java
@@ -4,19 +4,19 @@
 package somesexyshit;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
+
 import javax.swing.JPanel;
 import javax.swing.JProgressBar;
 import javax.swing.JTextField;
 import javax.swing.UIManager;
 import javax.swing.UIManager.LookAndFeelInfo;
 
-import java.awt.LayoutManager;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
-import java.util.Arrays;
+
 
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
