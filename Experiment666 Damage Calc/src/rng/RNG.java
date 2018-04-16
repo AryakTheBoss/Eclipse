327c0e28e3588a625798761c0b95d41207ca7cba
parya <parya@Mak-Surface>
2018-04-16 16:35:45 -0400

    Added RNG for drops

---
+++ Experiment666 Damage Calc/src/rng/RNG.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package rng;
+
+import java.util.Random;
+
+/**
+ * @author parya
+ *
+ */
+public class RNG implements Runnable{
+	
+	private int currentRNG = 0;
+	private Random r = new Random();
+	
+	public RNG() {
+		
+		new Thread(this).start();
+		
+	}
+	public int getCurrentRNG() {
+		return currentRNG;
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		for(;;) {
+			currentRNG = r.nextInt(65536);
+		}
+		
+	}
+	
+
+}
